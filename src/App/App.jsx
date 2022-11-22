b82f6e1bf50f62f6fa206287c761c235b05507c5
Volodymyr Kurov <vokur13@gmail.com>
2022-11-22 15:31:12 +0200

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -40,8 +40,8 @@ export class App extends Component {
       number,
     };
 
-    const checkName = this.state.contacts.some(item =>
-      item.name.toLowerCase().includes(contact.name.toLowerCase())
+    const checkName = this.state.contacts.some(
+      item => item.name.toLowerCase() === contact.name.toLowerCase()
     );
 
     checkName
@@ -49,14 +49,6 @@ export class App extends Component {
       : this.setState(({ contacts }) => ({
           contacts: [contact, ...contacts],
         }));
-
-    //     this.setState(({ contacts }) => ({
-    //       contacts: contacts.some(item =>
-    //         item.name.toLowerCase().includes(contact.name.toLowerCase())
-    //           ? alert(`${contact.name} is already in contacts`)
-    //           : [contact, ...contacts]
-    //       ),
-    //     }));
   };
 
   changeFilter = e => {
